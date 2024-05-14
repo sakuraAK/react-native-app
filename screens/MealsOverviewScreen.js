1e271a208a2b0ffaa220ef6e610cd4529a539ee6
sakuraAK <andrey.krutauz@gmail.com>
2024-05-14 18:51:17 -0400

    MealsOverviewScreen first try

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,13 @@
 import { MEALS, CATEGORIES } from "../data/dummy-data";
-import { View, Text } from "react-native";
+import { View, Text, List, FlatList} from "react-native";
 import { useEffect, useLayoutEffect } from "react";
+import MealItem from "../components/MealDetails";
 
 
 export default function MealsOverviewScreen({ route, navigation }) {
     const categoryId = route.params.categoryId;
+
+    const meals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) !== -1);
 
     useLayoutEffect(() => {
         const categoryTitle = CATEGORIES.find(
@@ -18,7 +21,11 @@ export default function MealsOverviewScreen({ route, navigation }) {
 
     return (
         <View>
-            <Text>Meals Overview for category - {categoryId} </Text>
+           <FlatList
+                data={meals}
+                keyExtractor={item => item.id}
+                renderItem={itemData => (<Text>{itemData.item.title}</Text>)}
+           />
         </View>
     );
 
